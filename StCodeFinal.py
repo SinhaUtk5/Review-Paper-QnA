1442db259e650eaddb9cafdd2b68549a661b3e53
SinhaUtk5 <118255174+SinhaUtk5@users.noreply.github.com>
2026-05-06 20:21:16 +0530

    Update StCodeFinal.py

--- StCodeFinal.py
+++ StCodeFinal.py
@@ -53,15 +53,15 @@ st.set_page_config(
 )
 
 # --- Top logos ---------------------------------------------------------------------
-left_space, logo1, logo2, right_space = st.columns([2, 1, 1, 2])
+left_space, logo1, logo2, right_space = st.columns([1, 1, 1, 1])
 
 with logo1:
     if os.path.exists("TAMU.png"):
-        st.image("TAMU.png", width=210)
+        st.image("TAMU.png", width=200)
 
 with logo2:
     if os.path.exists("IPBF.png"):
-        st.image("IPBF.png", width=210)
+        st.image("IPBF.png", width=200)
 
 
 
@@ -371,7 +371,7 @@ with c1:
     st.markdown(
         """
         **Utkarsh Sinha**<br>
-        Volunteer Research Associate <br>
+        Volunteer Research Fellow <br>
         Interaction of Phase-Behavior and Flow (IPB&F) Consortium<br>
         """,
         unsafe_allow_html=True,
